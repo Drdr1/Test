03649ac4cfb7ea45162472bad7db1b2886121e89
ahmed darder <91543865+Drdr1@users.noreply.github.com>
2024-10-11 18:25:33 +0300

    Update run.py

--- run.py
+++ run.py
@@ -7,8 +7,8 @@ from flask_migrate import Migrate
 from sys import exit
 from decouple import config
 
-from apps.config import config_dict
-from apps import create_app, db
+from test.config import config_dict
+from test import create_app, db
 
 # WARNING: Don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
